c2a0112a3cee650716c280413671388c5a1c27b6
heidi1105 <heidi1105@gmail.com>
2022-04-15 12:16:02 -0700

    log-reg

---
+++ demo/Full Stack/log-reg-demo/src/main/java/com/heidichen/logregdemo/LogRegDemoApplication.java
@@ -0,0 +1,13 @@
+package com.heidichen.logregdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogRegDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogRegDemoApplication.class, args);
+	}
+
+}
